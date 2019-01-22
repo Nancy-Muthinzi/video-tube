0f05f55eee5e623a4a78c398de4c20fee333af6f
Nancy Muthinzi <nanciekathini@gmail.com>
2019-01-22 15:54:14 +0300

    create superuser

--- tube/models.py
+++ tube/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 25)
+    last_name = models.CharField(max_length = 25)
+    email = models.EmailField()
+
