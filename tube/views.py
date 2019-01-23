aca0d303dc9a23ee9d4ba0cb1559288edf39af15
Nancy Muthinzi <nanciekathini@gmail.com>
2019-01-23 17:19:40 +0300

    create login and logout functionality

--- tube/views.py
+++ tube/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 import datetime as dt 
 
 # Create your views here.
 def welcome(request):
     date = dt.date.today()
     return render(request, 'welcome.html', {"date":date})
+
+@login_required(login_url='/accounts/login/')
+def video(request, video_id):
+        try:
+            video = Video.objects.get(id = video_id)
+    
+        except DoesNotExist:
+            raise Http404()
+            
+        return render(request, "video.html", {"video":video})
 
 def search_results(request):
     if 'video' in request.GET and request.GET["video"]:
